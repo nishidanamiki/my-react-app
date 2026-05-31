f2a91cf500ec8725d4b218da1c412cf97f2ca748
nishida namiki <ymdnmk1201@gmail.com>
2026-05-31 16:42:32 +0900

    feat: UserProfileコンポーネントを追加

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,11 @@
+function UserProfile() {
+  return (
+    <div className="user-profile">
+      <p>名前: 山田 太郎</p>
+      <p>趣味: プログラミング</p>
+      <hr />
+    </div>
+  );
+}
+
+export default UserProfile;
