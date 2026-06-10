493001e29844fd45a3f89ef1a889759fc8093e43
nishida namiki <ymdnmk1201@gmail.com>
2026-06-10 16:34:57 +0900

    feat: クリーンアップ関数を使ったタイマーを追加

--- src/components/EffectPractice.tsx
+++ src/components/EffectPractice.tsx
@@ -1,7 +1,28 @@
 import { useEffect, useState } from "react";
 
+function Timer() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    console.log("タイマー開始");
+
+    const intervalId = setInterval(() => {
+      console.log("チクタク");
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    return () => {
+      console.log("タイマー停止");
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <p>time: {time}</p>;
+}
+
 function EffectPractice() {
   const [count, setCount] = useState(0);
+  const [showTimer, setShowTimer] = useState(true);
 
   useEffect(() => {
     console.log("最初だけ実行");
@@ -17,8 +38,20 @@ function EffectPractice() {
 
   return (
     <div>
+      <h2>useEffectの基本練習</h2>
+
       <p>count: {count}</p>
       <button onClick={() => setCount(count + 1)}>増やす</button>
+
+      <hr />
+
+      <h2>クリーンアップ関数の練習</h2>
+
+      <button onClick={() => setShowTimer(!showTimer)}>
+        タイマー表示/非表示
+      </button>
+
+      {showTimer && <Timer />}
     </div>
   );
 }
